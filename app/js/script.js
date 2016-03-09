730ecb8ad51a5eab0db8606c165a114f6730b231
athira <athira@qburst.com>
2016-03-09 10:41:52 +0530

    Added filter by technology feature

--- app/js/script.js
+++ app/js/script.js
@@ -25,5 +25,49 @@ $(document).ready(function(){
 			$(".least-gallery li:last a img").attr("src",currentProject.img);
 		}
 		$(".least-gallery li a").attr("data-subtitle","View Project Details");
+		for(j=0;j<techarr.length;j++){
+			$(".fpselect").append("<option>"+tech[j]+"</option>");
+		}
+		$(".fpselect").change(function(){
+			var selectedText = $(this).find("option:selected").text();
+    		filterTech(selectedText,projects,arr);
+		});
 	});
 });
+function displayProjects(projects){
+	var length = projects.length;
+	for(i=0;i<length;i++){
+			$(".least-gallery").append('<li><a><img/></a></li>');
+			var currentThumbnail = $(".least-gallery li:last a");
+			currentThumbnail.attr({
+				href: projects[i].img,
+				title: projects[i].name			
+			});
+			currentThumbnail.attr("data-caption",projects[i].desc);
+			$(".least-gallery li:last a img").attr("src",projects[i].img);
+	}
+	$(".least-gallery li a").attr("data-subtitle","View Project Details");
+	$('.least-gallery').least();
+}
+function filterTech(tech,projects,arr){
+	var t = [];
+	if(tech=="All"){
+		$(".least-gallery li").remove();
+		for(i=0;i<arr.length;i++){
+			t.push(projects[i].project);
+		}
+	}
+	else{
+		for(i=0;i<arr.length;i++){
+			var technologies = projects[i].project.tech.split(",");
+			for(j=0;j<technologies.length;j++){
+				if(technologies[j].trim()==tech.trim()){
+					t.push(projects[i].project);
+				}
+			}
+		}
+	}
+		$(".least-gallery li").remove();
+		displayProjects(t);
+	
+}
